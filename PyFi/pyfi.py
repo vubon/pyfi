2c7bcfc1eddd78762ed45352bef5732a211cd7a6
vubon <vubon.roy@gmail.com>
2020-07-08 16:27:26 +0600

    - Add code docs - Rename main class name

--- PyFi/pyfi.py
+++ PyFi/pyfi.py
@@ -1,6 +1,9 @@
 """
-Prototype: Bla bla bla
+Short Description: PyFi is a short form. Py means Python, Fi means File and Wifi.
+PyFi is a file sharing project over WiFi network.
 
+@Author: Vubon Roy
+@since: Jun, 2020
 """
 import sys
 import argparse
@@ -9,7 +12,7 @@ from PyFi.utils import *
 from PyFi.server import run_server
 
 
-class PySender:
+class PyFi:
 
     def __init__(self):
         self.__ip = get_ip_address()
@@ -29,9 +32,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="File name or directory name with full path or run where is existing the file")
     args = parser.parse_args()
-    sender = PySender()
+    sender = PyFi()
     sender.run("/" + args.file)
-    # sender.run(path='/home/vubon/personal/PyFi/dist/dog.jpg')
 
 
 if __name__ == "__main__":
